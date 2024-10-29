595bdfaf1358c223954ae49cabd53cc8c5f50a57
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-29 01:52:29 +0100

    button

--- src/lib/components/examples/Ex001Button.svelte
+++ src/lib/components/examples/Ex001Button.svelte
@@ -2,9 +2,9 @@
 	let count = $state(0);
 	let double = $derived(count * 2);
 
-	function increment() {
+	const increment = () => {
 		count += 1;
-	}
+	};
 </script>
 
 <p class="mb-2">This is a button that increases a count by 2 each time it is pressed.</p>
