c54c8af69c9b6b189a2798e71803843d4c075ae7
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-05 07:52:28 +0100

    fixed await

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -6,6 +6,8 @@ export const numberOfSkills = writable<number | null>(null);
 user.set({ name: 'Robert', loggedIn: false });
 
 // load async
-const response = await fetch('https://edwardtanguay.vercel.app/share/skills.json');
-const _numberOfSkills = await response.json();
-numberOfSkills.set(_numberOfSkills.length);
+(async () => {
+	const response = await fetch('https://edwardtanguay.vercel.app/share/skills.json');
+	const _numberOfSkills = await response.json();
+	numberOfSkills.set(_numberOfSkills.length);
+})();
