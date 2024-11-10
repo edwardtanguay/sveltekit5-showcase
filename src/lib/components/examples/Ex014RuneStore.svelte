84fbfe4ee13640fa18c4f4140a71a1556b284d5d
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-10 02:42:11 +0100

    loading

--- src/lib/components/examples/Ex014RuneStore.svelte
+++ src/lib/components/examples/Ex014RuneStore.svelte
@@ -6,5 +6,21 @@
 
 <section>
 	<p class="mb-3">count: <span class="text-warning-500">{store.count}</span></p>
-	<button type="button" onclick={() => store.resetCount()} class="variant-filled-primary btn mb-3">reset</button>
+	<button type="button" onclick={() => store.resetCount()} class="variant-filled-primary btn mb-3"
+		>reset</button
+	>
+	<div>
+		<button type="button" onclick={() => store.loadSkills()} class="variant-filled-primary btn mb-3"
+			>load skills</button
+		>
+	</div>
+	{#if store.loadingSkills}
+		<p class="mb-3">
+			<span class="text-warning-500">{store.loadingSkills ? 'loading skills...' : ''}</span>
+		</p>
+	{:else}
+		<p class="mb-3">
+			number of skills: <span class="text-warning-500">{store.numberOfSkills}</span>
+		</p>
+	{/if}
 </section>
--- src/stores/runeStore.svelte.ts
+++ src/stores/runeStore.svelte.ts
@@ -1,11 +1,19 @@
 let count = $state(0);
 const doubleCount = $derived(count * 2);
+let numberOfSkills = $state(0);
+let loadingSkills = $state(false);
 
 export const getStore = () => {
 	return {
 		// values
 		get count() {
 			return count;
+		},
+		get numberOfSkills() {
+			return numberOfSkills;
+		},
+		get loadingSkills() {
+			return loadingSkills;
 		},
 
 		// computed values
@@ -16,6 +24,20 @@ export const getStore = () => {
 		// actions
 		incrementCount: () => count++,
 		decrementCount: () => count--,
-		resetCount: () => (count = 0)
+		resetCount: () => {
+			count = 0;
+			numberOfSkills = 0;
+		},
+
+		// thunks
+		loadSkills: () => {
+			loadingSkills = true;
+			setTimeout(async () => {
+				const response = await fetch('https://edwardtanguay.vercel.app/share/skills.json');
+				const skills = await response.json();
+				numberOfSkills = skills.length;
+				loadingSkills = false;
+			}, 2000);
+		}
 	};
 };
