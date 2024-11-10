722b901ee67498c83f3fca31ea134064922d1aa8
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-10 02:23:28 +0100

    actions

--- src/lib/components/examples/Ex013RuneStore.svelte
+++ src/lib/components/examples/Ex013RuneStore.svelte
@@ -6,6 +6,7 @@
 
 <section>
 	<p class="mb-3">count: <span class="text-warning-500">{store.count}</span></p>
+	<p class="mb-3">double count: <span class="text-warning-500">{store.doubleCount}</span></p>
 	<button type="button" onclick={() => store.decrementCount()} class="variant-filled-primary btn mb-3">decrease</button>
 	<button type="button" onclick={() => store.incrementCount()} class="variant-filled-primary btn mb-3">increase</button>
 </section>
--- src/stores/runeStore.svelte.ts
+++ src/stores/runeStore.svelte.ts
@@ -1,12 +1,21 @@
 let count = $state(0);
+const doubleCount = $derived(count * 2);
 
 export const getStore = () => {
 	return {
+		// values
 		get count() {
 			return count;
 		},
+
+		// computed values
+		get doubleCount() {
+			return doubleCount;
+		},
+
+		// actions
 		incrementCount: () => count++,
 		decrementCount: () => count--,
-		resetCount: () => count = 0
+		resetCount: () => (count = 0)
 	};
 };
