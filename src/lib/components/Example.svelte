e71c2fdefc16708d41373b28e8721405c514a486
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-14 07:39:46 +0100

    example fixed

--- src/lib/components/Example.svelte
+++ src/lib/components/Example.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
 	import { PUBLIC_SITE_ENV } from '$env/static/public';
 
-	export let title = 'nnn';
-	export let idCode = '';
-	export let publish = true;
+	const { title = 'nnn', idCode = '', publish = true } = $props();
 
 	const url = `https://github.com/edwardtanguay/sveltekit5-showcase/blob/dev/src/lib/components/examples/${idCode}.svelte`;
 </script>
 
 {#if PUBLIC_SITE_ENV === 'develop' || publish}
-	<div class="card mt-6 variant-gradient-primary">
+	<div class="variant-gradient-primary card mt-6">
 		<header class="card-header text-secondary-300">{title}</header>
 		<section class="p-4"><slot /></section>
 		<footer class="card-footer">
