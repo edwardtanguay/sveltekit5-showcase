a1e799913806176f64405008518945be3369ac5a
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-15 20:51:08 +0100

    name is avail

--- src/lib/components/examples/Ex019Checkmark.svelte
+++ src/lib/components/examples/Ex019Checkmark.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
 	let isAvailable = $state(true);
+	let firstName = $state('nnn');
 </script>
 
-<section class="w-[13rem]">
+<section>
 	<form>
-		<input type="checkbox" bind:checked={isAvailable} />
-		{#if isAvailable}
-			we have it in stock
-		{:else}
-			it's out of stock
-		{/if}
+		<div class="flex gap-1">
+			<label for="firstName">Name:</label>
+			<input id="firstName" type="text" bind:value={firstName} />
+		</div>
+		<div class="flex gap-1">
+			<input id="isAvailable" type="checkbox" bind:checked={isAvailable} />
+			<label for="isAvailable">is available</label>
+		</div>
 	</form>
 </section>
-
-
-
