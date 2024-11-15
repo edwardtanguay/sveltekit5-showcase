cd12ea41f13ea535f60808021c69693f3f868d5b
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-15 21:01:28 +0100

    finished

--- src/lib/components/examples/Ex019Checkmark.svelte
+++ src/lib/components/examples/Ex019Checkmark.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
 	const formData = $state({
 		isAvailable: false,
-		lastName: ''
+		firstName: ''
 	});
 </script>
 
 <section>
 	<form>
 		<div class="flex gap-1">
-			<label for="name">Last name:</label>
-			<input id="name" type="text" bind:value={formData.lastName} />
+			<label for="name">First name:</label>
+			<input id="name" type="text" bind:value={formData.firstName} />
 		</div>
 		<div class="flex gap-1">
 			<input id="isAvailable" type="checkbox" bind:checked={formData.isAvailable} />
 			<label for="isAvailable">is available</label>
 		</div>
 	</form>
-	<p>{formData.lastName} {formData.isAvailable ? 'is currently available' : 'is not available at the moment'}.</p>
+	{#if formData.firstName !== ""}
+	<p>{formData.firstName} {formData.isAvailable ? 'is currently available' : 'is not available at the moment'}.</p>
+	{/if}
 </section>
